883761a2d9fc76de50d32652925a5c0fce1f50bf
Geiden93 <geiden93@gmail.com>
2021-04-10 18:37:23 +0200

    sezione creata, inizio staff cards

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,12 @@
+<footer class="custom-footer mt-5 container-fluid">
+    <div class="container"> 
+    <div class="row">
+        <div class="text-center col-md-6 col-12">
+            <p>Studi Dentistici Dr. Matteo Frozzi - Viale del Mercato Nuovo, 44F – 36100 VICENZA</p>
+        </div>
+        <div class="text-center col-md-6 col-12">
+            <p>P.IVA 01502620386 • PRIVACY • COOKIE • CREDITS</p>
+        </div>
+    </div>
+</div>
+</footer>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,11 +2,12 @@
 
     <x-navbar />
 
-    <div class="container-fluid center custom-header img">
+    <div class="container-fluid center custom-header img mb-5">
         <div class="row  justify-content-center">
             <div class="pt-5  col-8 col-md-6">
-                <h2>Nuvola® è l’allineatore invisibile senza fili né attacchi in metallo</h2>
-                <h5 class="custom-info">CI PRENDIAMO CURA DI TE CON TRATTAMENTI PERSONALIZZATI</h5>
+                <h2>Dent<span class=" article-color">Net®</span> è l’allineatore invisibile senza fili né attacchi in
+                    metallo</h2>
+                <p class="mt-3 custom-info">CI PRENDIAMO CURA DI TE CON TRATTAMENTI PERSONALIZZATI</p>
                 <a class="custom-info-button btn btn-primary" href="">Scopri di più</a>
             </div>
             <div class="col-md-4 m-5">
@@ -14,32 +15,74 @@
         </div>
     </div>
 
-
     <article>
-        <div class="container mb-5">
-            <h2>Trova la prestazione che fa per te</h2>
-            <p>I NOSTRI STUDI DENTISTICI SONO A VICENZA, FERRARA E CENTO</p>
+        <div class="container col-10 mb-5 col-md-12">
+            <h2 class="article-color">Trova la prestazione che fa per te</h2>
+            <p class="article-color mt-3 custom-info">I NOSTRI STUDI DENTISTICI SONO A VICENZA, FERRARA E CENTO</p>
         </div>
     </article>
 
-    <article>
-        <div class="container mb-5">
-            <h2>Trova la prestazione che fa per te</h2>
-            <p>I NOSTRI STUDI DENTISTICI SONO A VICENZA, FERRARA E CENTO</p>
-        </div>
-    </article>
+    <section>
+        <div class="container">
+            <div class="row">
+                <div class="img-section col-md-6 col-12">
+                    <img class=" img-fluid"
+                        src="https://www.matteofrozzi.it/wp-content/uploads/2015/05/studio-dentistico-frozzi-cento.jpg"
+                        alt="">
+                </div>
+                <div class="bg-primary custom-section col-md-6 col-12">
+                    <h2 class="mt-5">Rendi i denti più puliti</h2>
+                    <p class="mt-5">La bellezza di un sorriso sano comincia dalla prevenzione:
+                        solo così puoi evitare il formarsi della placca e del tartaro,
+                        scongiurando l’insorgere di carie, gengiviti e piorrea.</p>
+                    <a class="mt-5 custom-info-button btn btn-primary" href="">Scopri di più</a>
 
-    <footer class="custom-footer mt-5 container-fluid">
-        <div class="container"> 
-        <div class="row">
-            <div class="text-center col-md-6 col-12">
-                <p>Studi Dentistici Dr. Matteo Frozzi - Viale del Mercato Nuovo, 44F – 36100 VICENZA</p>
+                </div>
+                <div class="bg-custom-section custom-section col-md-6 col-12">
+                    <h2 class="mt-5">Fai splendere il tuo sorriso</h2>
+                    <p class="mt-5">Alcune sostanze come caffè, tè, vino rosso e fumo di sigaretta possono macchiare i
+                        tuoi denti,
+                        compromettendo il loro colore naturale.
+                        Le tecniche di sbiancamento eseguite negli Studi Dentistici Dr. Frozzi
+                        migliorano la tonalità originale e la lucentezza della tua dentatura.</p>
+                    <a class="mt-5 custom-info-button btn bg-primary btn-primary" href="">Scopri di più</a>
+                </div>
+                <div class="img-section col-md-6 col-12">
+                    <img class="img-fluid"
+                        src="https://www.matteofrozzi.it/wp-content/uploads/2015/05/studio-dentistico-frozzi-vicenza1.jpg"
+                        alt="">
+                </div>
+                <div class="img-section col-md-6 col-12">
+                    <img class=" img-fluid"
+                        src="https://www.matteofrozzi.it/wp-content/uploads/2015/05/studio-dentistico-frozzi-cento.jpg"
+                        alt="">
+                </div>
+                <div class="bg-primary custom-section col-md-6 col-12">
+                    <h2 class="mt-5">Un nuovo volto al tuo sorriso</h2>
+                    <p class="mt-5">Il sorriso racconta molto di te, del tuo carattere e del modo con cui affronti la
+                        vita; per questo è importante averne cura.
+                        I professionisti degli Studi Dentistici Dr.
+                        Frozzi partono da una presa in carico globale del paziente, considerando anche le componenti
+                        extradentali – in particolare labbra e gengive – che concorrono a definire ogni fisionomia.
+                        Inoltre eseguiamo interventi di chirurgia maxillo facciale e orbito palpebrale direttamente in
+                        studio.</p>
+                    <a class="mt-5 custom-info-button btn btn-primary" href="">Scopri di più</a>
+
+                </div>
+
+
+
             </div>
-            <div class="text-center col-md-6 col-12">
-                <p>P.IVA 01502620386 • PRIVACY • COOKIE • CREDITS</p>
-            </div>
+
         </div>
-    </div>
-    </footer>
+        </div>
+
+        </div>
+    </section>
+
+
+
+
+    <x-footer />
 
 </x-layout>
