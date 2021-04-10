d348826a026465959285253d6df28a26881cd2c7
Geiden93 <geiden93@gmail.com>
2021-04-10 17:28:49 +0200

    navbar e footer OK

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:wght@300&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;700&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
+    <title>Document</title>
+</head>
+<body>
+    
+
+    {{$slot}}
+
+    <script src="{{asset('js/app.js')}}"></script>
+</body>
+</html>
---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,24 @@
+
+
+<nav class="navbar navbar-expand-lg  custom-navbar  align-items-center">
+    <div class="container-fluid">
+      <div class="d flex  ">
+        <a class="navbar-brand" href="#">DR FOZZI</a>
+        <p class="custom-info-navbar">STUDI DENTISTICI</p>
+      </div>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse row" id="navbarNavAltMarkup">
+       
+          <div class="m-3 navbar-nav ">
+            <a class="col-1 custom-info-navbar nav-link" href="#">STAFF</a>
+            <a class="col-1 custom-info-navbar nav-link" href="#">PERCHE' SCEGLIERCI</a>
+            <a class="col-1 custom-info-navbar nav-link" href="#">DOVE SIAMO</a>
+            <a class="col-1 custom-info-navbar nav-link" href="#">CONTATTI</a>
+            <a class="ms-5 p-1 btn btn-primary col-2 custom-info-navbar" href="#">PRENOTA VISITA</a>
+
+        </div>
+      </div>
+    </div>
+  </nav>
